731aa127b619e13c6ffef6bfe6b63693cc90d5ff
innacp <inna.chapkevych@gmail.com>
2021-11-12 13:17:32 +0200

    12.11

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+let openButtonClass = ".open-modal-window";
+let closeButtonClass = ".modal-close-icon";
+let modalContainerClass = ".modal-menu-container";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let openButton = document.querySelector(openButtonClass);
+let closeButton = document.querySelector(closeButtonClass);
+
+let modalContainer = document.querySelector(modalContainerClass);
+
+openButton.addEventListener("click", (el) => {
+  modalContainer.classList.toggle("show");
+});
+
+closeButton.addEventListener("click", (el) => {
+  modalContainer.classList.toggle("show");
+});
+
